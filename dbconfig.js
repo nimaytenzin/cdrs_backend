cc73998c389337d1d54322a4f6f84e5539773cc2
nimaytenzin <nimaytenzin@gmail.com>
2022-10-13 16:44:18 +0600

    ok

--- dbconfig.js
+++ dbconfig.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
-const pass = encodeURIComponent("covid@2020#")
+const pass = encodeURIComponent("*Dhs@2022#")
 
-const connectionString = `postgresql://postgres:${pass}@172.30.20.22:5432/cdrs`;
+const connectionString = `postgresql://postgres:${pass}@192.168.20.87:5432/cdrs`;
 
 
 const pool = new Pool({
