b5f6cfc21cf07db6e35fff6eb5bf7950c7aef976
nimaytenzin <nimaytenzin@gmail.com>
2022-10-18 00:13:34 +0600

    ok

--- routes/index.js
+++ routes/index.js
@@ -180,9 +180,8 @@ router.put('/api/proposals/set-done/:gid', (req, res) => {
 })
 
 router.put('/api/proposals/updateRemarks', (req, res) => {
- 
   pool.query(`
-  UPDATE proposals_shape SET remarks = ${req.body.remarks} WHERE  gid = ${Number(req.body.fid)}
+  UPDATE proposals_shape SET remarks = '${req.body.remarks}' WHERE  gid = ${Number(req.body.fid)}
   `, (err, ress) => {
     if (err) {
       throw err
